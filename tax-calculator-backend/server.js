8c4d1e31cf2d5f1af3b46dca1d1132ba323f005a
Rammurat <rammuratyadav224@gmail.com>
2025-02-17 19:50:24 +0530

    Initial commit

--- tax-calculator-backend/server.js
+++ tax-calculator-backend/server.js
@@ -79,7 +79,7 @@ app.get("*", (_, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
