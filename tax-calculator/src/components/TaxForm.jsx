0d7d8442ef7fc6db2153406a0c8a02858ff12026
Rammurat <rammuratyadav224@gmail.com>
2025-02-17 15:14:44 +0530

    Initial commit

--- tax-calculator/src/components/TaxForm.jsx
+++ tax-calculator/src/components/TaxForm.jsx
@@ -19,7 +19,7 @@ const TaxForm = ({ setTaxResult }) => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:5000/calculate-tax",
+        "http://localhost:3206/calculate-tax",
         formData
       );
       setTaxResult(response.data);
